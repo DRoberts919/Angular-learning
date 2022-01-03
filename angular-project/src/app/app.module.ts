502a5a493f7e2fff35e1fb169b03c5036bce6e16
Dylan <60489717+DRoberts919@users.noreply.github.com>
2022-01-03 11:29:53 -0700

    creation of subcomponent
    created a subcomponent and passed data from the parent to the child. this needed to some work due to the video being slightly dated

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ArtistItemsComponent } from './artist-items/artist-items.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, ArtistItemsComponent ],
+	imports: [ BrowserModule, FormsModule, NgbModule, HttpClientModule ],
+	providers: [],
+	bootstrap: [ AppComponent ],
+	
 })
-export class AppModule { }
+export class AppModule {}
